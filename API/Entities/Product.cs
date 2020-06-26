5454673f1d594e7036adc4b9f293c349ab263557
Егор Плужник <egor.pluzhnil.lol@gmail.com>
2020-06-26 08:53:57 +0500

    add migrations

---
+++ API/Entities/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Threading.Tasks;
+
+namespace API.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
