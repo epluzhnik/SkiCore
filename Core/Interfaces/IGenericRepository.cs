df8d346d189f698a8dbf6bd4afff763776d7766e
Egor Pluzhnik <egor.pluzhnik.lol@gmail.com>
2020-11-01 19:48:46 +0500

    add specification evaluator

--- Core/Interfaces/IGenericRepository.cs
+++ Core/Interfaces/IGenericRepository.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using Core.Entities;
+using Core.Specifications;
 
 namespace Core.Interfaces
 {
@@ -9,5 +10,7 @@ namespace Core.Interfaces
     {
         Task<T> GetByIdAsync(int id);
         Task<IReadOnlyList<T>> GetListAllAsync();
+        Task<T> GetEntityWithSpec(ISpecification<T> specification);
+        Task<IReadOnlyList<T>> ListAsync(ISpecification<T> spec);
     }
 }
