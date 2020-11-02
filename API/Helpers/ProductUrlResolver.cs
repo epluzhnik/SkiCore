6fb2db1c8bd051e0795f505e9b315fb1dd03278d
Egor Pluzhnik <egor.pluzhnik.lol@gmail.com>
2020-11-02 20:37:16 +0500

    add resolver for picture url

---
+++ API/Helpers/ProductUrlResolver.cs
@@ -0,0 +1,28 @@
+﻿using API.Dtos;
+using AutoMapper;
+using Core.Entities;
+using Microsoft.Extensions.Configuration;
+
+namespace API.Helpers
+{
+    public class ProductUrlResolver : IValueResolver<Product, ProductToReturn, string>
+    {
+        private readonly IConfiguration config;
+
+        public ProductUrlResolver(IConfiguration config)
+        {
+            this.config = config;
+        }
+        
+        public string Resolve(Product source, ProductToReturn destination, string destMember, 
+            ResolutionContext context)
+        {
+            if (!string.IsNullOrEmpty(source.PictureUrl))
+            {
+                return config["ApiUrl"] + source.PictureUrl;
+            }
+
+            return null;
+        }
+    }
+}
