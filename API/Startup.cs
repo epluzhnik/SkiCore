933db594ee267dbff8a61f2bf432aea2b3d2b668
Egor Pluzhnik <egor.pluzhnik.lol@gmail.com>
2020-11-03 21:29:24 +0500

    added cors support

--- API/Startup.cs
+++ API/Startup.cs
@@ -30,6 +30,14 @@ namespace API
             services.AddApplicationServices();
             
             services.AddSwaggerDocumentations();
+
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+                });
+            });
         }
         
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -42,6 +50,8 @@ namespace API
 
             app.UseRouting();
             app.UseStaticFiles();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
