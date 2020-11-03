f3d7fe3e69d4ab2f06437db652e0776f6bf09742
Egor Pluzhnik <egor.pluzhnik.lol@gmail.com>
2020-11-03 16:30:51 +0500

    implemented validation error

---
+++ API/Errors/ApiValidationErrorResponse.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace API.Errors
+{
+    public class ApiValidationErrorResponse : ApiResponse
+    {
+        public ApiValidationErrorResponse(int statusCode, string message = null) : base(statusCode, message)
+        {
+        }
+
+        public ApiValidationErrorResponse() : base(400)
+        {
+
+        }
+
+        public IEnumerable<string> Errors { get; set; }
+    }
+}
--- API/Startup.cs
+++ API/Startup.cs
@@ -1,3 +1,5 @@
+using System.Linq;
+using API.Errors;
 using API.Helpers;
 using API.Middleware;
 using AutoMapper;
@@ -5,6 +7,7 @@ using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -31,6 +34,24 @@ namespace API
 
             services.AddScoped<IProductRepository, ProductRepository>();
             services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+
+            services.Configure<ApiBehaviorOptions>(options =>
+            {
+                options.InvalidModelStateResponseFactory = actionContext =>
+                {
+                    var errors = actionContext.ModelState
+                        .Where(e => e.Value.Errors.Count > 0)
+                        .SelectMany(x => x.Value.Errors)
+                        .Select(x => x.ErrorMessage).ToArray();
+
+                    var errorResponse = new ApiValidationErrorResponse
+                    {
+                        Errors = errors
+                    };
+                    
+                    return new BadRequestObjectResult(errorResponse);
+                };
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
