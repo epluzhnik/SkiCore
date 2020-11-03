0bdbbbb430cc03f91598e089b57a9e23994cba79
Egor Pluzhnik <egor.pluzhnik.lol@gmail.com>
2020-11-03 16:54:04 +0500

    added swagger

--- API/Startup.cs
+++ API/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace API
 {
@@ -52,6 +53,11 @@ namespace API
                     return new BadRequestObjectResult(errorResponse);
                 };
             });
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo{ Title = "SkiCore API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -68,6 +74,10 @@ namespace API
             app.UseStaticFiles();
 
             app.UseAuthorization();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => 
+                { c.SwaggerEndpoint("/swagger/v1/swagger.json", "SkiCore API v1"); });
 
             app.UseEndpoints(endpoints =>
             {
